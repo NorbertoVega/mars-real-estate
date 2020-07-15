3411b9d03435c96c5a87ba4e8c183769e1f97c4a
norberto <norberto.a.vega@gmail.com>
2020-07-14 23:27:05 -0300

    Starter Code

---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    private fun getMarsRealEstateProperties() {
+        _response.value = "Set the Mars API Response here!"
+    }
+}
